86b8af20911b5aa49f05b8c3b95615f5029aadc7
850252329 <850252329@qq.com>
2019-09-05 23:26:41 +0800

    cvte

--- src/mianshi/CVTE0905Test1.java
+++ src/mianshi/CVTE0905Test1.java
@@ -1,25 +1,26 @@
 package mianshi;
 
-
 /*
  *	2019/9/05 CVTE 笔试算法1
  *特殊计数器，开始计数12345 12345678910 123...20 ...
  *计数器的最大值每次乘二，输入一个数，返回计数器的值
  *如输入1 返回1，输入15返回10 
+ *
+ *ac
  */
 public class CVTE0905Test1 {
 
 	public static void main(String[] args) {
 		System.out.println(getTime(15));
 	}
-	
+
 	public static int getTime(int n) {
 		int count = 5;
-		while(count < n) {
-			n=n-count;
-			count*=2;
+		while (count < n) {
+			n = n - count;
+			count *= 2;
 		}
-		
+
 		return n;
 	}
 
---
+++ src/mianshi/CVTE0905Test2.java
@@ -0,0 +1,45 @@
+package mianshi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+ * 求两个数组中第二大的值，没有返回-1
+ * ac
+ */
+public class CVTE0905Test2 {
+
+	public static void main(String[] args) {
+		List<Integer> list1 = new ArrayList<>();
+		List<Integer> list2 = new ArrayList<>();
+		list1.add(100);
+		list1.add(90);
+		list1.add(90);
+		list1.add(100);
+		list2.add(100);
+		list2.add(90);
+		list2.add(90);
+		list2.add(100);
+		int a = getNumberTwo(list1, list2);
+		System.out.println(a);
+	}
+
+	private static int getNumberTwo(List<Integer> list1, List<Integer> list2) {
+		list1.addAll(list2);
+		Collections.sort(list1);
+		if (list1.size() <= 1)
+			return -1;
+		int res = list1.get(list1.size() - 1);
+		for (int i = list1.size() - 2; i >= 0; i--) {
+			if (res > list1.get(i)) {
+				res = list1.get(i);
+				break;
+			}
+		}
+		if (res == list1.get(list1.size() - 1))
+			return -1;
+		return res;
+	}
+
+}
