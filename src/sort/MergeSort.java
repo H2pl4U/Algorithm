cf57c8039cfa3e5539338cd8096dc0d952683165
850252329 <850252329@qq.com>
2019-09-21 22:18:38 +0800

    merge

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -2,56 +2,66 @@ package sort;
 
 /**
  * 并归排序
+ * 
  * @author ilovejava1314
  *
  */
 public class MergeSort {
 
 	public static void main(String[] args) {
-		int[] a ={12,20,5,26,35,1,30,45,23,9};
-		int start = 0;
-		int end =a.length-1;
-		sort(a,start,end);
-		for(int i = 0;i<a.length;i++){
-			System.out.println(a[i]);
+		int[] arr = { 12, 20, 5, 26, 35, 1, 30, 45, 23, 9 };
+		int L = 0;
+		int R = arr.length - 1;
+		mergeSort(arr, L, R);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
 		}
 	}
 
-	public static int[] sort(int[] a,int low,int high){
-        int mid = (low+high)/2;
-        if(low<high){
-            sort(a,low,mid);
-            sort(a,mid+1,high);
-            //左右归并
-            merge(a,low,mid,high);
-        }
-        return a;
-    }
-	
-	public static void merge(int[] a, int low, int mid, int high) {
-        int[] temp = new int[high-low+1];
-        int i= low;
-        int j = mid+1;
-        int k=0;
-        // 把较小的数先移到新数组中
-        while(i<=mid && j<=high){
-            if(a[i]<a[j]){
-                temp[k++] = a[i++];
-            }else{
-                temp[k++] = a[j++];
-            }
-        }
-        // 把左边剩余的数移入数组 
-        while(i<=mid){
-            temp[k++] = a[i++];
-        }
-        // 把右边边剩余的数移入数组
-        while(j<=high){
-            temp[k++] = a[j++];
-        }
-        // 把新数组中的数覆盖nums数组
-        for(int x=0;x<temp.length;x++){
-            a[x+low] = temp[x];
-        }
-    }
+	private static void mergeSort(int[] arr, int l, int r) {
+		if (l == r)
+			return;
+		else {
+			int m = (l + r) / 2;
+			mergeSort(arr, l, m);
+			mergeSort(arr, m + 1, r);
+			merge(arr, l, m + 1, r);
+		}
+	}
+
+	// 二路归并
+	private static void merge(int[] arr, int l, int m, int r) {
+		int leftSize = m - l;
+		int rightSize = r - m + 1;
+		// 数组切分为l-m和m+1-r两部分
+		int[] left = new int[leftSize];
+		int[] right = new int[rightSize];
+		// 给left数组赋值
+		for (int i = l; i < m; i++) {
+			left[i - l] = arr[i];
+		}
+		// 给right数组赋值
+		for (int i = m; i <= r; i++) {
+			right[i - m] = arr[i];
+		}
+		// i指向left，j指向right，k指向arr
+		int i = 0, j = 0, k = l;
+		// 把较小的数先移到新数组中
+		while (i < leftSize && j < rightSize) {
+			if (left[i] < right[j]) {
+				arr[k++] = left[i++];
+			} else {
+				arr[k++] = right[j++];
+			}
+		}
+		// 把左边剩余的数移入数组
+		while (i < leftSize) {
+			arr[k++] = left[i++];
+		}
+		// 把右边剩余的数移入数组
+		while (i < rightSize) {
+			arr[k++] = right[j++];
+		}
+	}
+
 }
