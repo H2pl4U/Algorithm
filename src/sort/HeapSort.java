7839a82cebefe0405c3a53590b553e7823b6b9c9
850252329 <850252329@qq.com>
2019-09-23 12:41:18 +0800

    heap

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -2,8 +2,7 @@ package sort;
 
 /**
  * 堆排序
- * @author ilovejava1314 
- *
+ * @author ilovejava1314
  */
 public class HeapSort {
 
@@ -15,37 +14,57 @@ public class HeapSort {
 		}
 	}
 
+	//堆排序函数 
 	public static void headSort(int[] list) {
-		// 构造初始堆,从第一个非叶子节点开始调整,左右孩子节点中较大的交换到父节点中
-		for (int i = (list.length) / 2 - 1; i >= 0; i--) {
-			headAdjust(list, list.length, i);
+		buildHeap(list);
+		//堆顶是最大元素,从尾遍历,每次将头尾交换
+		for (int i = list.length-1; i >= 0; i--) {
+			swap(list, i, 0);
+			//调整堆(不管最大元素)
+			heapAdjust(list, i, 0);
 		}
-		// 排序，将最大的节点放在堆尾，然后从根节点重新调整
-		for (int i = list.length - 1; i >= 1; i--) {
-			int temp = list[0];
-			list[0] = list[i];
-			list[i] = temp;
-			headAdjust(list, i, 0);
+	}
+	
+	//创建堆函数
+	public static void buildHeap(int[] list) {
+		int len = list.length;
+		//找到最后一个叶子结点的父节点
+		int parent = (len - 2) / 2;
+		//从该节点开始遍历调整
+		for (int i = parent; i >= 0; i--) {
+			heapAdjust(list, len, i);
 		}
 	}
 
-	private static void headAdjust(int[] list, int len, int i) {
-		int k = i, temp = list[i], index = 2 * k + 1;
-		while (index < len) {
-			if (index + 1 < len) {
-				if (list[index] < list[index + 1]) {
-					index = index + 1;
-				}
-			}
-			if (list[index] > temp) {
-				list[k] = list[index];
-				k = index;
-				index = 2 * k + 1;
-			} else {
-				break;
-			}
+	//调整堆函数
+	private static void heapAdjust(int[] list, int len, int i) {
+		if (i >= len)
+			return;
+		// 左子树
+		int c1 = 2 * i + 1;
+		// 右子树
+		int c2 = 2 * i + 2;
+		int max = i;
+		//找出这部分最大值
+		if (c1 < len && list[c1] > list[max]) {
+			max = c1;
 		}
-		list[k] = temp;
+		if (c2 < len && list[c2] > list[max]) {
+			max = c2;
+		}
+		//将最大值与根位置互换
+		if (max != i) {
+			swap(list, max, i);
+			heapAdjust(list, len, max);
+		}
+
+	}
+
+	//交换函数
+	private static void swap(int[] list, int max, int i) {
+		int temp = list[max];
+		list[max] = list[i];
+		list[i] = temp;
 	}
 
 }
