3d0c1e02e422c625e94b529a2fae42dbab5a3d06
850252329 <850252329@qq.com>
2019-09-23 22:45:37 +0800

    sort

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -2,46 +2,47 @@ package sort;
 
 /**
  * ¿ìËÙÅÅĞò
+ * 
  * @author ilovejava1314
  *
  */
 public class QuickSort {
-	
+
 	public static void main(String[] args) {
-		int[] a ={12,20,5,26,35,1,30,45,23,9};
+		int[] a = { 12, 20, 5, 26, 35, 1, 30, 45, 23, 9 };
 		int start = 0;
-		int end =a.length-1;
-		sort(a,start,end); 
-		for(int i = 0;i<a.length;i++){
+		int end = a.length - 1;
+		quickSort(a, start, end);
+		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
-		
 
 	}
 
-	private static void sort(int[] a, int low, int high) {
+	private static void quickSort(int[] a, int low, int high) {
 		int start = low;
 		int end = high;
 		int key = a[start];
-		while(end>start){
-			while(end>start&&a[end]>=key)
-				end--;
-			if(a[end]<=key){
+		while (end > start) {
+			while (end > start && a[end] >= key)
+			end--;
+			if (a[end] <= key) {
 				int temp = a[end];
 				a[end] = a[start];
 				a[start] = temp;
 			}
-			while(end>start&&a[start]<=key)
+			while (end > start && a[start] <= key)
 				start++;
-			if(a[start]>=key) {
+			if (a[start] >= key) {
 				int temp = a[start];
 				a[start] = a[end];
 				a[end] = temp;
 			}
 		}
-		if(start>low) sort(a,low,start-1);
-		if(end<high) sort(a,end+1,high);
-		
+		if (start > low)
+			quickSort(a, low, start - 1);
+		if (end < high)
+			quickSort(a, end + 1, high);
 	}
 
 }
