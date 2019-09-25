a525760d14efc4bc73a6e44ba0ab251ce87aeaa5
850252329 <850252329@qq.com>
2019-09-25 23:37:06 +0800

    Thread

---
+++ src/bishi/畅游笔试编程2.java
@@ -0,0 +1,57 @@
+package bishi;
+
+class MyThread1 implements Runnable {
+	public void run() {
+		int count = 0;
+		for (int i = 1; i <= 52; i++) {
+			count++;
+			synchronized (this) {
+				if (count == 2) {
+					try {
+						this.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					count = 0;
+				}else {
+					System.out.println(i);
+					this.notify();
+				}
+			}
+		}
+	}
+}
+
+class MyThread2 implements Runnable {
+
+	public void run() {
+		int count = 0;
+		for (char i = 'A'; i <= 'Z'; i++) {
+			count++;
+			synchronized (this) {
+				if (count == 1) {
+					try {
+						this.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					count = 0;
+				}else {
+					System.out.println(i);
+					this.notify();
+				}
+			}
+		}
+	}
+}
+
+public class ³©ÓÎ±ÊÊÔ±à³Ì2 {
+	public static void main(String[] args) {
+		MyThread1 t1 = new MyThread1();
+		MyThread2 t2 = new MyThread2();
+		Thread thread1 = new Thread(t1);
+		Thread thread2 = new Thread(t2);
+		thread1.start();
+		thread2.start();
+	}
+}
