f90ab59b9e04afe749626853ce1ac475a5c7040d
850252329 <850252329@qq.com>
2019-09-27 10:52:21 +0800

    360

--- src/bishi/三六零笔试编程T2.java
+++ src/bishi/三六零笔试编程T2.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class ÈıÁùÁã±ÊÊÔ±à³ÌT2 {
 
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
