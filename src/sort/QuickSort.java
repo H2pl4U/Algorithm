63da0529cb4d55bbf73feb947ecd25c2611478c9
850252329 <850252329@qq.com>
2020-03-05 19:41:53 +0800

    cmt

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -9,10 +9,10 @@ package sort;
 public class QuickSort {
 
 	public static void main(String[] args) {
-		int[] a = { 12, 20, 5, 26, 35, 1, 30, 45, 23, 9 };
+		int[] a = { 12, 20, 5, 26, 35, 1, 30, 45,5, 23, 9 };
 		int start = 0;
 		int end = a.length - 1;
-		quickSort(a, start, end);
+		quickSort2(a, start, end);
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
@@ -24,19 +24,17 @@ public class QuickSort {
 		int end = high;
 		int key = a[start];
 		while (end > start) {
+			//从后往前扫描，找出比第一个元素小的索引
 			while (end > start && a[end] >= key)
-			end--;
-			if (a[end] <= key) {
-				int temp = a[end];
-				a[end] = a[start];
-				a[start] = temp;
+				end--;
+			//如果确实小于则交换值
+			if (a[end] < key) {
+				swap(a,start,end);
 			}
 			while (end > start && a[start] <= key)
 				start++;
-			if (a[start] >= key) {
-				int temp = a[start];
-				a[start] = a[end];
-				a[end] = temp;
+			if (a[start] > key) {
+				swap(a,start,end);
 			}
 		}
 		if (start > low)
@@ -44,5 +42,38 @@ public class QuickSort {
 		if (end < high)
 			quickSort(a, end + 1, high);
 	}
+
+	private static void swap(int[] a, int start, int end) {
+		int temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+	}
+	
+	
+	private static void quickSort2(int[] a, int low, int high) {
+		int start = low,end = high;
+		int num = a[low];
+		while(start < end) {
+			while(start < end && a[end] >= num)
+				end--;
+			if(a[end] < num) {
+				swap(a,start,end);
+			}
+			while(start < end && a[start] <= num)
+				start++;
+			if(a[start] > num) {
+				swap(a,start,end);
+			}
+			if(start > low) {
+				quickSort2(a, low, start-1);
+			}
+			if(end < high) {
+				quickSort2(a, end+1, high);
+			}
+		}
+	}
+	
+	
+	
 
 }
